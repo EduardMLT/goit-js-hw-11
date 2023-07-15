dd93bdfea62f6b6d80b5064e3cf82782b62382b3
EduardMLT <eddku@i.ua>
2023-07-16 02:20:58 +0300

    initial

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+'use strict';
+import { GalleryAPI } from './js/input-api';
+import Notiflix from 'notiflix';
 import "simplelightbox/dist/simple-lightbox.min.css"; 
 import SimpleLightbox from 'simplelightbox'; 
 
@@ -9,20 +12,29 @@ const userList = document.querySelector(".user-list");
 const searchForm = document.querySelector(".search-form");
 const btnLoadMore = document.querySelector('.load-more');
 
+const galleryInstance = new GalleryAPI();
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
+});
+
 btnLoadMore.style.display = 'none';
 
 function onSearch(e) {
    console.log(`3-query: ${query}`);
    e.preventDefault();
     
-   query = e.currentTarget.elements.query.value;
+  query = e.currentTarget.elements.query.value;
+  galleryInstance.q = query;
+  galleryInstance.page = 1;
+  
   
   if (query === '') {
     return alert('Enter the word to search ...');
     };    
     console.log(`4-пошук: ${query}`);
   // e.currentTarget.reset();
-  
+   
    fetchUsers()
     .then((users) => renderUserList(users))
     .catch((error) => console.log(error));
@@ -32,7 +44,7 @@ function onSearch(e) {
 
 function fetchUsers() {   
     console.log(`5-пошук: ${query}`);    
-  const url = `https://pixabay.com/api/?key=38232376-4840eb4d2a32943b9bc00372c&q=${query}&image_type=photo&per_page=6&page=${page}`
+  const url = `https://pixabay.com/api/?key=38232376-4840eb4d2a32943b9bc00372c&q=${query}&image_type=photo&per_page=6&page=${page}&orientation=horizontal`
   console.log(`5-URL: ${url}`);
   
   return fetch(url).then(
@@ -48,43 +60,111 @@ function fetchUsers() {
 }
 
 function renderUserList(users) {   
+    
+  userList.innerHTML = createGalleryCards(users);
+  lightbox.refresh();
+  btnLoadMore.style.display = 'block';
+    
+}
+
+// function onBtnLoadMoreClick() {
+//   console.log("galleryInstance= ", galleryInstance.fetchImages());
+//   page += 1;
+//   fetchUsers(page);
+// }
+ 
+function onBtnLoadMoreClick() {
+  galleryInstance.page += 1;
+  console.log("galleryInstance= ", galleryInstance.fetchImages());
+  galleryInstance.fetchImages().then(data => {
+    if (data.data.total === data.data.totalHits) {
+      btnLoadMore.style.display = 'none';
+      onFetchInfo();
+    }
+    console.log("data data= ", data);
+    userList.insertAdjacentHTML(
+      'beforeend',
+      newGreateGalleryCards(data.data.hits)
+    );
+    lightbox.refresh();
+  });
+}
+
+function onFetchInfo() {
+  Notiflix.Report.info(
+    'We are sorry',
+    'but you have reached the end of search results.',
+    'Okay',
+    {
+      position: 'center-center',
+      timeout: 5000,
+      width: '400px',
+      titleFontSize: '30px',
+      messageFontSize: '20px',
+    }
+  );
+}
+
+function createGalleryCards(users) {
     console.log(`7-пошук: ${query}`,users.hits);
     // e.preventDefault();
   const markup = users.hits
     .map((user) => {
         return `<div class="gallery_card">
           <a class="gallery_info" href="${user.largeImageURL}">
-            <img class="gallery_image" src="${user.webformatURL}" alt="">
+            <img class="gallery_image" src="${user.webformatURL}" alt="${user.tags}">
+          </a>
               <div class="gallery_list">
                  <p class="gallery_item"><b class="gallery_item_info">likes</b>${user.likes}</p>
                  <p class="gallery_item"><b class="gallery_item_info">views</b>${user.views}</p>
                  <p class="gallery_item"><b class="gallery_item_info">comments</b>${user.comments}</p>          
                  <p class="gallery_item"><b class="gallery_item_info">downloads</b>${user.downloads}</p>
-              </div>
-          </a>
+              </div>          
         </div>`;
     })
     .join("");
-  userList.innerHTML = markup;
-  
-  btnLoadMore.style.display = 'block';
-
-    const lightbox = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
-});
+  return markup;
 }
 
-function onBtnLoadMoreClick() {
-  
-  page += 1;
-  // onSearch();
-  fetchUsers(page);
-//   renderUserList();  
- }
+function newGreateGalleryCards(arr) {
+  return arr
+    .map(
+      ({
+        largeImageURL,
+        webformatURL,        
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `<div class="gallery_card">
+  <a class="gallery_info" href="${largeImageURL}">
+    <img class="gallery_image" src="${webformatURL}" alt="${tags}"/>
+      </a>
+  <div class="gallery_list">
+      <p class="gallery_item">
+        <b class="gallery_item_info">Likes</b>${likes}
+      </p>
+      <p class="gallery_item">
+        <b class="gallery_item_info">Views</b>${views}
+      </p>
+      <p class="gallery_item">
+        <b class="gallery_item_info">Comments</b>${comments}
+      </p>
+      <p class="gallery_item">
+        <b class="gallery_item_info">Downloads</b>${downloads}
+      </p>
+    </div>
+</div>`
+    )
+    .join('');   
+}
 
 searchForm.addEventListener("submit", onSearch);
 console.log(`0-query: ${query}`);
    
 btnLoadMore.addEventListener('click', onBtnLoadMoreClick);
+
+
   
 
---
+++ src/js/input-api.js
@@ -0,0 +1,22 @@
+'use strict';
+import axios from 'axios';
+
+export class GalleryAPI {
+  #API_KEY = '38232376-4840eb4d2a32943b9bc00372c';
+  #BASE_URL = `https://pixabay.com/api/`;
+  page = 1;
+  q = null;
+
+  async fetchImages() {
+    return await axios.get(`${this.#BASE_URL}`, {
+      params: {
+        q: this.q,
+        key: this.#API_KEY,
+        page: this.page,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        per_page: 6,
+      },
+    });
+  }
+}
